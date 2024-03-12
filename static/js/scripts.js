2ec80b5e9e3d5b1c8deb480786a98773081e6287
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-03-12 15:55:55 +0000

    navBar responsive

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,13 @@
 const sliderBtn = document.querySelector("#slider");
 const styleCss = document.documentElement.style;
+const menu = document.querySelector(".menu ion-icon");
+const navBar = document.querySelector(".navBar");
+const ul = document.querySelector('.navBar ul ');
+const all_link = ul.querySelectorAll('a');
+const span = ul.querySelectorAll('span');
+
+
+
 if (localStorage.getItem('sliderState')==='checked'){
     sliderBtn.checked = true;
     document.body.classList.add("dark-theme");
@@ -11,8 +19,40 @@ if (localStorage.getItem('sliderState')==='checked'){
 sliderBtn.addEventListener('change', function(){
   
     localStorage.setItem('sliderState', this.checked ? 'checked': 'unchecked');
-    document.body.classList.toggle(("dark-theme"))
+    document.body.classList.toggle(("dark-theme"));
    
-})
+});
+
+ 
+menu.onclick = function(){
+    navBar.classList.toggle("navBar-res");
+    all_link.forEach((a)=>{
+        a.classList.toggle('link');
+    });
+    span.forEach((sp)=>{
+        sp.classList.toggle("span")
+    })
+    
+}
+var menuName = menu.getAttribute('name')
+/* <ion-icon name="close-outline"></ion-icon> */
+menu.addEventListener('click', ()=>{
+    
+   if(menu.getAttribute('name')==='menu-outline'){
+    menu.setAttribute('name', 'close-outline');
+    menu.style.transform = "translateX(400px)"
+    menu.style.fontSize = "30px";
+    
+   }
+
+   else{
+   
+    menu.setAttribute('name','menu-outline');
+    menu.style.transform = "translateX(0)";
+    
+   };
+   
+});
+    
 
 
