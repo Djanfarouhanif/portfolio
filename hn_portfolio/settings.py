2e40e5917940c97a3cf7d02d43608c8b2049882a
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-03-22 23:00:10 +0000

    add message from portfolio

--- hn_portfolio/settings.py
+++ hn_portfolio/settings.py
@@ -133,4 +133,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "hanifdjanfarou8@gmail.com"
-EMAIL_HOST_PASSWORD = 'Maria9297'
+EMAIL_HOST_PASSWORD = 'ciuk jh nt qpuh ptmw'
--- hn_profile/views.py
+++ hn_profile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-
+from django.contrib  import messages
 from .models import Content,Project
 
 def index(request):
